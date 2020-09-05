e6e93c71cc2dde54b24809adf640719612d338e2
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-09-05 12:39:30 +0530

    upto OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '613721862495289',
+        clientSecret: '872a81a20eaad70d7a6c9a4c2e180473'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -56,7 +56,19 @@ router.post('/signup', (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     res.json({success: true, token:token ,status: 'You are successfully logged in!'});
   });
+
   
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
+
+
   router.get('/logout', (req, res) => {
     if (req.session) {
       req.session.destroy();
